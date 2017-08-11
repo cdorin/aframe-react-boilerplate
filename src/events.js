8dc3779288ab00d32bb96cfdf74de45543349fb7
cdorin <camila.dorin@gmail.com>
2017-08-11 18:10:39 +0300

    last changes to menu, working

--- src/events.js
+++ src/events.js
@@ -1,21 +1,65 @@
 
-AFRAME.registerComponent('set-color', {
+window.AFRAME.registerComponent('set-color1', {
         dependencies: ['material'],
         init: function() {
-          var bigbox = document.querySelector('#entity--box--menu4');
+          var bigbox = document.querySelector('#big-box');
           this.el.addEventListener('click', () => {
-            bigbox.el.setAttribute('material', 'color', '#00008B');
-          });
-        }
-      });
-     
-       
-AFRAME.registerComponent('set-sky', {
-        init: function() {
-          var sky = document.querySelector('a-sky');
-          this.el.addEventListener('click', () => {
-            sky.setAttribute('src', this.data);
+            bigbox.setAttribute('material', 'color', getRandomColor());
           });
         }
       });
 
+window.AFRAME.registerComponent('set-color2', {
+        dependencies: ['material'],
+        init: function() {
+          var bigbox = document.querySelector('#small-box');
+          this.el.addEventListener('click', () => {
+            bigbox.setAttribute('material', 'color', getRandomColor());
+          });
+        }
+      });	  
+       
+window.AFRAME.registerComponent('set-particle', {
+       
+        init: function() {
+          var particles = document.querySelector('#particles');
+          this.el.addEventListener('click', () => {
+            particles.setAttribute('particle-system', 'particle-count', getRandomNum());
+          });
+        }
+      });
+
+window.AFRAME.registerComponent('set-sky', {
+        init: function() {
+          var sky = document.querySelector('#the-sky');
+          this.el.addEventListener('click', () => {
+			if(sky.getAttribute('src') == '#skypic2')
+			{
+				sky.setAttribute('src', '#skypic1');
+			}
+			else 
+			{
+				sky.setAttribute('src', '#skypic2');
+			}
+          });
+        }
+      });
+
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getRandomNum() {
+  const letters = '0123456789ABCDEF';
+  var num = '#';
+  for (var i = 0; i < 20; i++ ) {
+    num += letters[Math.floor(Math.random() * 100)];
+  }
+  return num;
+}
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-require('events');
+require('./events');
 
 class App extends React.Component {
   constructor(props) {
@@ -27,26 +27,14 @@ class App extends React.Component {
         <a-assets>
           <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
           <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
+		  <img id="skypic1" src="https://c3.staticflickr.com/2/1475/26239222850_cabde81c39_k.jpg"/>
+		  <img id="skypic2" src="https://c2.staticflickr.com/2/1688/25044226823_53c979f8a1_k.jpg"/>
         </a-assets>
 
-        <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
         <Entity primitive="a-light" type="ambient" color="#445451"/>
         <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
-        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-        <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
-        <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
 
-        <Entity id="box"
-          geometry={{primitive: 'box'}}
-          material={{color: this.state.color, opacity: 0.6}}
-          animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-          animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-          position={{x: 0, y: 1, z: -3}}
-          events={{click: this.changeColor.bind(this)}}>
-          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
-                  geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
-                  material={{color: '#24CAFF'}}/>
-        </Entity>
+        
 		
 		<Entity id="entity--plane--menu"
 		  geometry={{primitive: 'plane', height: 1, width: 3.5}}
@@ -58,14 +46,16 @@ class App extends React.Component {
 		  geometry={{primitive: 'box'}}
 		  position= {{x: -1.1, y: 2.4, z: -3}}
 		  scale = {{x: 0.5, y: 0.5, z: 0.1}}
-		  material={{color: '#DC143C'}}>
+		  material={{color: '#DC143C'}}
+		  set-sky >
 		</Entity>
 		
 		<Entity id="entity--box--menu2"
 		  geometry={{primitive: 'box'}}
 		  position= {{x: -0.3, y: 2.4, z: -3}}
 		  scale = {{x: 0.5, y: 0.5, z: 0.1}}
-		  material={{color: '#00FFFF'}}>
+		  material={{color: '#00FFFF'}}
+		  set-particle >
 		</Entity>
 		
 		<Entity id="entity--box--menu3"
@@ -73,7 +63,7 @@ class App extends React.Component {
 		  position= {{x: 0.5, y: 2.4, z: -3}}
 		  scale = {{x: 0.5, y: 0.5, z: 0.1}}
 		  material={{color: '#00008B'}}
-		  >
+		  set-color1 >
 		</Entity>
 		
 		<Entity id="entity--box--menu4"
@@ -81,9 +71,23 @@ class App extends React.Component {
 		  position= {{x: 1.3, y: 2.4, z: -3}}
 		  scale = {{x: 0.5, y: 0.5, z: 0.1}}
 		  material={{color: '#FF8C00'}}	
-		  set-color >
+		  set-color2 >
 		</Entity>
 		 
+		<Entity id="big-box"
+          geometry={{primitive: 'box'}}
+          material={{color: '#24CAFF', opacity: 0.6}}
+          animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
+          animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
+          position={{x: 0, y: 1, z: -3}}>
+          <Entity id="small-box" animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
+                  geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
+                  material={{color: '#24CAFF'}}/>
+        </Entity>
+		
+		<Entity id="the-sky" primitive="a-sky" src="#skypic1" />
+
+		<Entity id="particles" particle-system={{preset: 'snow', particleCount: 2000}}/>
 
         <Entity primitive="a-camera">
           <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
